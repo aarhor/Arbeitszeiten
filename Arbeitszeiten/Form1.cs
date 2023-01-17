67a8b58b31e123ddbc68c2c71f14177cd9ef0b40
aarhor <30049374+aarhor@users.noreply.github.com>
2023-01-17 22:50:54 +0100

    Kommandozeilenargumente hinzugefügt

--- Arbeitszeiten/Form1.cs
+++ Arbeitszeiten/Form1.cs
@@ -1,3 +1,7 @@
+//using Arbeitszeiten;
+
+using static Arbeitszeiten.Program;
+
 namespace Arbeitszeiten
 {
     public partial class Form1 : Form
@@ -5,6 +9,14 @@ namespace Arbeitszeiten
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string firstArgument = CommandLineArguments.Args[0];
+
+
+            label1.Text = firstArgument;
         }
     }
 }
--- Arbeitszeiten/Program.cs
+++ Arbeitszeiten/Program.cs
@@ -2,6 +2,32 @@ namespace Arbeitszeiten
 {
     internal static class Program
     {
+        public static class CommandLineArguments
+        {
+            public static string[] Args { get; private set; }
+
+            static CommandLineArguments()
+            {
+                Args = CheckCMDArgs(Environment.GetCommandLineArgs());
+            }
+        }
+
+        public static string[] CheckCMDArgs(string[] Argumente)
+        {
+            string[] allowedArguments = new string[] { "Anmelden", "Abmelden"};
+
+            List<string> validArguments = new List<string>();
+            foreach (string arg in Argumente)
+            {
+                if (allowedArguments.Contains(arg))
+                {
+                    validArguments.Add(arg);
+                }
+            }
+
+            return validArguments.ToArray();
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -10,6 +36,8 @@ namespace Arbeitszeiten
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
