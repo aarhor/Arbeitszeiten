2d5619b43e9ef365396bf0f9115374709d9babde
aarhor <30049374+aarhor@users.noreply.github.com>
2023-03-14 13:30:07 +0100

    Datenbanken erstellung überarbeitet

--- Arbeitszeiten/Resources/Erstellen.sql
+++ Arbeitszeiten/Resources/Erstellen.sql
@@ -1,4 +1,4 @@
 ﻿CREATE TABLE Taetigkeiten (_id INTEGER PRIMARY KEY AUTOINCREMENT, Datum date, Uhrzeit datetime, Tätigkeit TEXT);
 CREATE TABLE Zeiten (_id INTEGER PRIMARY KEY AUTOINCREMENT, Datum DATE, Start DATETIME, Ende DATETIME, Differenz DOUBLE, MehrMinder_Stunden DOUBLE, Bemerkung TEXT default NULL);
-CREATE VIEW 'geordnet' AS select * from "Zeiten" order by Datum, Start;
-CREATE VIEW 'Überstunden' AS select round(sum(MehrMinder_Stunden), 2) as Überstunden from "Zeiten";
+CREATE VIEW 'view_geordnet' AS select * from "Zeiten" order by Datum, Start;
+CREATE VIEW 'view_überstunden' AS select round(sum(MehrMinder_Stunden), 2) as Überstunden from "Zeiten";
